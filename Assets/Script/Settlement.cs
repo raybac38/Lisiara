de2a6fea51aeb740762e471610a0b7758b24767d
Hugo PONSIN <hponsin@proton.me>
2025-09-14 19:48:27 +0200

    architecture

---
+++ Assets/Script/Settlement.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settlement : MonoBehaviour
+{
+    private List<Settler> settlers = new();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
